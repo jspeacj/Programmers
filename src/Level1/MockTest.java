05f52500cf12acda1bad29e82df4c7dfbb168668
jspeacj <jspeacj@naver.com>
2022-06-27 18:19:37 +0900

    level1 init setting

---
+++ src/Level1/MockTest.java
@@ -0,0 +1,10 @@
+package Level1;
+
+public class MockTest {
+    public static void main(String[] args) {
+        int[] answers = new int[]{1, 2, 3, 4, 5};
+        int[] result = {};
+
+        System.out.println(result);
+    }
+}
