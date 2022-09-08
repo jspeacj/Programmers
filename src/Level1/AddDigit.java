4134d148f630c1bc0d0327dc83c4726fb90b82dd
jspeacj <jspeacj@naver.com>
2022-09-08 15:01:50 +0900

    자리수 더하기(Level 1) success

---
+++ src/Level1/AddDigit.java
@@ -0,0 +1,17 @@
+package Level1;
+
+public class AddDigit {
+    //자리수 더하기
+    public static void main(String[] args) {
+        int n = 987;
+
+        int answer = 0;
+        String[] arrayNum = String.valueOf(n).split("");
+
+        for (String s : arrayNum) {
+            answer += Integer.parseInt(s);
+        }
+
+        System.out.println(answer);
+    }
+}
