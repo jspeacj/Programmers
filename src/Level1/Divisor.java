4e764147a4878bfce73e74fac1e6c7c7819ee9da
jspeacj <jspeacj@naver.com>
2022-09-10 21:34:42 +0900

    약수 구하기 success

--- src/Level1/Divisor.java
+++ src/Level1/Divisor.java
@@ -3,18 +3,26 @@ package Level1;
 public class Divisor {
     //약수 구하기
     public static void main(String[] args) {
-        int n = 12;
+        int n = 5;
 
         int answer = 1; // 약수의 합
         int num = 2;
 
         while(true) {
-            System.out.println();
-            if (n % num == 0) answer += num;
+            if (n == 1) break;
+
+            if (n == 0) {
+                answer = 0;
+                break;
+            }
 
             if (num == n) {
                 answer += num;
                 break;
+            }
+
+            if (n % num == 0) {
+                answer += num;
             }
 
             num++;
