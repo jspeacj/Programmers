4afc773eca7e4560f57284b86f90f75564856307
jspeacj <jspeacj@naver.com>
2022-09-13 00:47:13 +0900

    정수 내림차순으로 배치하기 success

---
+++ src/Level1/IntegerDescending.java
@@ -0,0 +1,15 @@
+package Level1;
+
+import java.util.*;
+
+public class IntegerDescending {
+    public static void main(String[] args) {
+        //정수 내림차순으로 배치하기
+        long n = 118372;
+
+        String[] split = Long.toString(n).split("");
+        Arrays.sort(split, Comparator.reverseOrder());
+
+        System.out.println(Long.parseLong(String.join("", split)));
+    }
+}
