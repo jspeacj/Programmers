c02a0d3906c6becade6f429f7c10a93ebcbf771c
jspeacj <jspeacj@naver.com>
2022-09-30 15:23:21 +0900

    String to Date test

--- src/Level1/Test.java
+++ src/Level1/Test.java
@@ -1,15 +1,27 @@
 package Level1;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         /*
             해당 파일은 프로그래머스에 제출하기 전, 주석문을 제거하기 위해 만든 파일이다. (제출 전 임시 저장용)
          */
+        String scantoFrom = "202209012";
+
+        SimpleDateFormat sdfr = new SimpleDateFormat("yyyyMMdd");
+        Date date = sdfr.parse(scantoFrom);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+
+        cal.add(Calendar.DATE, 9);
+        System.out.println(cal.getTime());
+
+        String abc = sdfr.format(cal.getTime());
+        abc = abc +"235959";
+        System.out.println(abc);
     }
 }
