5ea5c4aced1445207b8098dd6ba1ebe408c0120b
jspeacj <jspeacj@naver.com>
2023-01-19 16:39:25 +0900

    (Level 2)n^2 배열 자르기 success!

--- src/Level2/NTwoCutArray.java
+++ src/Level2/NTwoCutArray.java
@@ -1,5 +1,7 @@
 package Level2;
 
+import java.util.Arrays;
+
 public class NTwoCutArray {
     public static void main(String[] args) {
         /*
@@ -15,8 +17,8 @@ public class NTwoCutArray {
             정수 n, left, right가 매개변수로 주어집니다. 주어진 과정대로 만들어진 1차원 배열을 return 하도록 solution 함수를 완성해주세요.
 
             제한사항
-            1 ≤ n ≤ 107
-            0 ≤ left ≤ right < n2
+            1 ≤ n ≤ 10(7)
+            0 ≤ left ≤ right < n(2)
             right - left < 105
             입출력 예
             n	    left	right	    result
@@ -35,43 +37,54 @@ public class NTwoCutArray {
                       2   2   3
                       3   3   3
 
-            1 2 3 4 8 15
-            2 2 3 4
-            3 3 3 4
-            4 4 4 4
+                      1 2 3 4
+            n : 4     2 2 3 4
+                      3 3 3 4
+                      4 4 4 4
 
         * */
         
         /*
         *  규칙 :
         *  1. 해당 2차원 배열의 첫번째 시작값은 행의 인덱스 + 1과 같다.
-        *  2. (left + 1) / n) : 시작 행의 위치 값
-        *  3. (left + 1) % n : 시작 열의 위치(0일 경우 값은 n이다.)
-        *  4. 현재 행의 위치 값 >= 현재 열의 위치 : 행의 위치 값
-        *  5. 현재 행의 위치 값 < 현재 열의 위치 : 현재 열의 위치 값
+        *  2. left / n : 시작 행의 좌표
+        *  3. (left % n : 시작 열의 좌표(0일 경우 값은 n이다.)
+        *  4. x의 좌표 / y의 좌표 중 큰 값 +1이 원하는 결과 값이다.
         * */
 
 
         /* TC 1  result : [3,2,2,3] */
-        int n = 3;
-        int left = 2;
-        int right = 5;
+        //int n = 3;
+        //long left = 2;
+        //long right = 5;
 
         /* TC 2  result : [4,3,3,3,4,4,4,4] */
-        //int n = 4;
-        //int left = 7;
-        //int right = 14;
+        int n = 4;
+        long left = 7;
+        long right = 14;
 
-        int[] result = new int[right - left + 1];
-        int strRow = (left + 1) / n;
-        int strCol = (left + 1) % n == 0? n : (left + 1) % n;
+        int[] result = new int[(int)(right - left) + 1];
+        //int strRow = (int)(left / n);
+        //int strCol = (int)(left % n) == 0 ? n : (int)(left % n);
 
-        System.out.println(strRow);
-        System.out.println(strCol);
+        for (int index = 0; index < result.length; index++) {
+            /* 채점 시 1,2번만 실패 (실패하는 원인을 모르겠음...)
+            int value = strRow > strCol ? strRow + 1 : strCol + 1;
+            result[index] = value;
 
-        for (int i = 0; i < result.length + 1; i++) {
-            if (strCol >= n) strCol = ++strRow;
+            if (strCol + 1 >= n) {
+                strRow++;
+                strCol = 0;
+            } else strCol++;
+            */
+
+            int strRow = (int)(left / n + 1);
+            int strCol = (int)(left % n + 1);
+            result[index] = Math.max(strRow,strCol);
+            left++;
 
         }
+
+        System.out.println(Arrays.toString(result));
     }
 }
