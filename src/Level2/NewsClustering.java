c2d82531174099ecfae7cf923587af912b1660b5
jspeacj <jspeacj@naver.com>
2023-01-25 19:02:56 +0900

    (Level 2)[1차] 뉴스 클러스터링 success!

--- src/Level2/NewsClustering.java
+++ src/Level2/NewsClustering.java
@@ -1,5 +1,9 @@
 package Level2;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class NewsClustering {
     public static void main(String[] args) {
         /*
@@ -41,6 +45,7 @@ public class NewsClustering {
             입력으로 들어온 문자열은 두 글자씩 끊어서 다중집합의 원소로 만든다. 이때 영문자로 된 글자 쌍만 유효하고, 기타 공백이나 숫자, 특수 문자가 들어있는 경우는 그 글자 쌍을 버린다.
             예를 들어 "ab+"가 입력으로 들어오면, "ab"만 다중집합의 원소로 삼고, "b+"는 버린다.
             다중집합 원소 사이를 비교할 때, 대문자와 소문자의 차이는 무시한다. "AB"와 "Ab", "ab"는 같은 원소로 취급한다.
+
             출력 형식
             입력으로 들어온 두 문자열의 자카드 유사도를 출력한다. 유사도 값은 0에서 1 사이의 실수이므로, 이를 다루기 쉽도록 65536을 곱한 후에 소수점 아래를 버리고 정수부만 출력한다.
 
@@ -51,5 +56,93 @@ public class NewsClustering {
             aa1+aa2	        AAAA12	    43690
             E=M*C^2	        e=m*c^2	    65536
          */
+
+        /*
+            ※ 문제 이해 및 풀기
+        * 1.입력으로 들어온 문자열은 두 글자씩 끊어서 다중집합의 원소로 만든다.
+        * 2.영문자로 된 글자 쌍만 유효하고, 기타 공백이나 숫자, 특수 문자가 들어있는 경우는 그 글자 쌍을 버린다.
+            (예를 들어 "ab+"가 입력으로 들어오면, "ab"만 다중집합의 원소로 삼고, "b+"는 버린다.)
+          3. A ∪ B = (A + B) - A ∩ B
+          4. 즉, 교집합(A ∩ B)을 구하면 합집합의 개수또한 구할 수 있다.
+          5. 마지막에 전달할 값 answer에다가 65536을 곱한다.
+          6. 곱한 값의 소수점아래는 버리고 반환한다.
+          7. ★ 집합 A와 집합 B가 모두 공집합일 경우에는 나눗셈이 정의되지 않으니 따로 J(A, B) = 1로 정의한다. ★
+        * */
+
+        /* TC 1 return : 16384 */
+        //String str1 = "FRANCE";
+        //String str2 = "french";
+
+        /* TC 2 return : 65536 */
+        //String str1 = "handshake";
+        //String str2 = "shake hands";
+
+        /* TC 3 return : 43690 */
+        //String str1 = "aa1+aa2";
+        //String str2 = "AAAA12";
+
+        /* TC 4 return : 65536 */
+        String str1 = "E=M*C^2";
+        String str2 = "e=m*c^2";
+
+        /* TC 5 return : 0 */
+        //String str1 = "A+C";
+        //String str2 = "DEF";
+
+        int emptyValue = 65536;
+        if (str1.trim().length() == 0 || str2.trim().length() == 0) System.out.println(emptyValue);
+
+        List<String> str1List = new ArrayList<>();
+        List<String> str2List = new ArrayList<>();
+        int intersection = 0; // 교집합
+        int union = 0; // 합집합
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        for (int i = 0; i < str1.length() - 1; i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str1.charAt(i+1);
+            if ((int)c1 < 97 || (int)c1 > 122) continue;
+            if ((int)c2 < 97 || (int)c2 > 122) continue;
+
+            str1List.add(str1.substring(i, i+2));
+        }
+
+        for (int i = 0; i < str2.length() - 1; i++) {
+            char c1 = str2.charAt(i);
+            char c2 = str2.charAt(i+1);
+            if ((int)c1 < 97 || (int)c1 > 122) continue;
+            if ((int)c2 < 97 || (int)c2 > 122) continue;
+
+            str2List.add(str2.substring(i, i+2));
+        }
+
+        if (str1List.size() == 0 && str2List.size() == 0) System.out.println(emptyValue);
+
+        union = str1List.size() + str2List.size();
+        str1List.sort(Comparator.reverseOrder());
+        str2List.sort(Comparator.reverseOrder());
+
+        for (String s1 : str1List) {
+            char c1 = s1.charAt(0);
+
+            for (String s2 : str2List) {
+                char c2 = s2.charAt(0);
+                if ((int)c2 > (int)c1) continue;
+                else if ((int)c2 < (int)c1) break;
+                else { // 첫번째 영문자가 같은 경우에만 수행
+                    if (s1.charAt(1) == s2.charAt(1)) {
+                        str2List.remove(s2);
+                        intersection++; // 교집합 증가
+                        break;
+                    }
+                }
+            }
+        }
+
+        int answer = (int)((double)intersection / (union - intersection) * 65536);
+
+        System.out.println(answer);
     }
 }
